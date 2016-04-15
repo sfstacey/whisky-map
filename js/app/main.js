d98adf0fcbdbbc051d701f0b462532f3c376c426
sfstacey <stacey.jenkins@screamingfrog.co.uk>
2016-04-15 16:30:47 +0100

    Trying to load the XML markers

--- js/app/main.js
+++ js/app/main.js
@@ -32,7 +32,58 @@ var map = new google.maps.Map(document.getElementById('whisky-map'), {
   mapTypeControl: false,
   streetViewControl: false,
 });
-
 map.mapTypes.set(customMapTypeId, customMapType);
 map.setMapTypeId(customMapTypeId);
+
+var xmlURL = "xml/distilleries.xml";
+loadMarkers(xmlURL, map);
+}
+
+function loadMarkers(xmlURL, map){
+var markers = [];
+downloadUrl(xmlURL, function(data){
+//var xml = data.reponseXML;
+var markers = xml.documentElement.getElementsByTagName('marker');
+
+for (var i = 0; i<markers.length; i++){
+            var name=markers[i].getAtrribute('name');
+            var marker_image = markers[i].getAttribute('markerimage');
+            var id = markers[i].getAttribute("id");
+            var address = markers[i].getAttribute("address1")+"<br />"+markers[i].getAttribute("address2")+"<br />"+markers[i].getAttribute("address3")+"<br />"+markers[i].getAttribute("postcode");
+            var image = {
+              url: marker_image,
+              size: new google.maps.Size(71, 132),
+              origin: new google.maps.Point(0, 0),
+              scaledSize: new google.maps.Size(71, 132)
+            };
+            var point = new google.maps.LatLng(
+                parseFloat(markers[i].getAttribute("lat")),
+                parseFloat(markers[i].getAttribute("lng")));
+            var html = "<div class='infowindow'><b>" + name + "</b> <br/>" + address+'<br/></div>';
+            var marker = new google.maps.Marker({
+              map: map,
+              position: point,
+              icon: image,
+              title: name
+            });
+            map.markers.push(marker);
+            bindInfoWindow(marker, map, infoWindow, html);
+        }
+    });
+};
+
+function downloadUrl(url,callback) {
+    var request = window.ActiveXObject ?
+         new ActiveXObject('Microsoft.XMLHTTP') :
+         new XMLHttpRequest;
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+            //request.onreadystatechange = doNothing;
+            callback(request, request.status);
+        }
+    };
+
+    request.open('GET', url, true);
+    request.send(null);
 }
